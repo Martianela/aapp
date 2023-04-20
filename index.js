b1d52ea62995f35792b0fc9caa3033d1cfd47e75
Martianela <sonirishabh2207@gmail.com>
2023-04-20 22:08:41 +0530

    jwtadded

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const userRoute = require('./routes/userRoute')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoute");
+const app = express();
+const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-mongoose.set('strictQuery', false)
-app.use('/user', userRoute)
+mongoose.set("strictQuery", false);
+app.use("/user", userRoute);
 
-
-mongoose.connect("mongodb+srv://saranshh2:0DFauPCVmJBFSGBu@cluster0.7cyagzw.mongodb.net/leetcode?retryWrites=true&w=majority",
+mongoose
+  .connect(
+    "mongodb+srv://saranshh2:0DFauPCVmJBFSGBu@cluster0.7cyagzw.mongodb.net/leetcode?retryWrites=true&w=majority",
     {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
 
-    })
-    .then(() => console.log("db connected"))
-    .catch((err) => console.log(err));
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
